76cc6be57ca08d986957ff8df0c6f2727da75141
magdy <magdysrag94@gmail.com>
2022-10-20 04:40:06 +0200

    video section and end animation of other sections

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function () {
+$(document).ready(function () {
   "use strict";
   // Adjust Slider Height
   var winH = $(window).height(),
@@ -18,6 +18,35 @@ $(function () {
   });
   $("[data-toggle='counter-up']").counterUp({
     delay: 10,
-    time: 2000
-});
+    time: 2000,
+  });
+  let endAbout = $(".about").offset().top + $(".about").height() +200;
+  let startAbout = $(".about").offset().top - 500;
+  $(window).on("scroll", function () {
+    let win = $(window).scrollTop();
+    if (win >= startAbout) {
+      if (win <= endAbout) {
+        $(".animation-img").addClass("zoomIn");
+        $(".img1").addClass("delay1");
+        $(".img2").addClass("delay2");
+        $(".img3").addClass("delay3");
+        $(".img4").addClass("delay4");
+      } else if (win > endAbout) {
+        $(".animation-img").removeClass("zoomIn").addClass("hidden");
+      }
+    } else {
+      $(".animation-img").removeClass("zoomIn").addClass("hidden");
+    }
+    if ($(window).scrollTop() >= $(".room").offset().top - 200) {
+      $(".main-head").addClass("delay0").addClass("fadeInUp");
+      $(".card-item1").addClass("fadeInUp");
+      $(".card-item2").addClass("delay1").addClass("fadeInUp");
+      $(".card-item3").addClass("delay2").addClass("fadeInUp");
+    } else {
+      $(".card-item").addClass("hidden");
+      $(".main-head").addClass("hidden");
+      $(".main-head").removeClass("fadeInUp");
+      $(".card-item").removeClass("fadeInUp");
+    }
+  });
 });
