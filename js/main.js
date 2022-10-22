99a4b95ad8fcaa17801f0a8acb5e640f86320f61
magdy <magdysrag94@gmail.com>
2022-10-22 05:35:38 +0200

    repear img in home section adding img-fluid finish video section fix animaton from down to top

--- js/main.js
+++ js/main.js
@@ -20,33 +20,37 @@ $(document).ready(function () {
     delay: 10,
     time: 2000,
   });
-  let endAbout = $(".about").offset().top + $(".about").height() +200;
+  let endAbout = $(".about").offset().top + $(".about").height() + 200;
   let startAbout = $(".about").offset().top - 500;
   $(window).on("scroll", function () {
     let win = $(window).scrollTop();
     if (win >= startAbout) {
-      if (win <= endAbout) {
         $(".animation-img").addClass("zoomIn");
         $(".img1").addClass("delay1");
         $(".img2").addClass("delay2");
         $(".img3").addClass("delay3");
         $(".img4").addClass("delay4");
-      } else if (win > endAbout) {
-        $(".animation-img").removeClass("zoomIn").addClass("hidden");
-      }
-    } else {
+    }
+    if (win < startAbout - 500) {
       $(".animation-img").removeClass("zoomIn").addClass("hidden");
     }
-    if ($(window).scrollTop() >= $(".room").offset().top - 200) {
+    if (win >= $(".room").offset().top - 200) {
       $(".main-head").addClass("delay0").addClass("fadeInUp");
       $(".card-item1").addClass("fadeInUp");
       $(".card-item2").addClass("delay1").addClass("fadeInUp");
       $(".card-item3").addClass("delay2").addClass("fadeInUp");
-    } else {
+    }
+    if (win < $(".room").offset().top - 650) {
       $(".card-item").addClass("hidden");
       $(".main-head").addClass("hidden");
       $(".main-head").removeClass("fadeInUp");
       $(".card-item").removeClass("fadeInUp");
+    }
+    if (win >= $(".video").offset().top - 400) {
+      $(".video").addClass("show");
+      $(".video").removeClass("hidden");
+    } else {
+      $(".video").removeClass("show").addClass("hidden");
     }
   });
 });
